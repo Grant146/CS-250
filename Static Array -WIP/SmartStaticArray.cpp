da061e843a6920c02701f71257983882d56d8875
Grant146 <grantmclain22@gmail.com>
2017-02-01 19:14:58 -0600

    Create SmartStaticArray.cpp

---
+++ Static Array -WIP/SmartStaticArray.cpp
@@ -0,0 +1,101 @@
+#include "SmartStaticArray.hpp"
+
+SmartStaticArray::SmartStaticArray()
+{
+	m_itemCount = 0;
+}
+
+bool SmartStaticArray::Push( const string& newItem )
+{
+	if (m_itemCount == MAX_SIZE)
+	{
+		return false; // temp
+	}
+
+	m_data[m_itemCount] = newItem;
+	m_itemCount++;
+}
+
+
+bool SmartStaticArray::Insert( int index, const string& newItem )
+{
+	if(m_itemCount > MAX_SIZE || m_itemCount < 0)
+	{
+		return false; // temp
+	}
+
+	for (int i = m_itemCount - 1; i >= index; i--)
+	{
+		m_data[i] == m_data[i - 1];
+
+	}
+
+
+}
+
+
+bool SmartStaticArray::Extend( const SmartStaticArray& other )
+{
+
+	return false; // temp
+}
+
+
+bool SmartStaticArray::Pop()
+{
+	return false; // temp
+}
+
+
+bool SmartStaticArray::Remove( int index )
+{
+	return false; // temp
+}
+
+
+string SmartStaticArray::Get( int index ) const
+{
+	return "temp"; // temp
+}
+
+
+int SmartStaticArray::Size() const
+{
+	return -1; // temp
+}
+
+
+bool SmartStaticArray::IsFull() const
+{
+	return false; // temp
+}
+
+bool SmartStaticArray::IsEmpty() const
+{
+	if (m_itemCount == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false; // temp
+	}
+}
+
+
+string SmartStaticArray::operator[]( int index )
+{
+	return "temp"; // temp
+}
+
+
+SmartStaticArray& SmartStaticArray::operator=( const SmartStaticArray& other )
+{
+    SmartStaticArray blorp; // temp
+    return blorp; // temp
+}
+
+bool SmartStaticArray::operator==( const SmartStaticArray& other )
+{
+	return false; // temp
+}
